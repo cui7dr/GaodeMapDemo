e7d61aca863c9a43106a5fe9066b62f6665c781b
cui <cui7dr@gmail.com>
2019-07-17 22:16:43 +0800

    定位界面

--- app/src/main/java/com/cui/gaodemapdemo/activity/PointActivity.java
+++ app/src/main/java/com/cui/gaodemapdemo/activity/PointActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 
 import com.amap.api.maps2d.AMap;
 import com.amap.api.maps2d.CameraUpdateFactory;
+import com.amap.api.maps2d.CoordinateConverter;
 import com.amap.api.maps2d.MapView;
 import com.amap.api.maps2d.model.LatLng;
 import com.amap.api.maps2d.model.Marker;
@@ -29,26 +30,44 @@ public class PointActivity extends AppCompatActivity {
     }
 
     // 初始化地图的控件
-    private void operationMap(@Nullable Bundle savedInstanceState){
+    private void operationMap(@Nullable Bundle savedInstanceState) {
         // 找到地图控件
-        mapView=(MapView)findViewById(R.id.map);
+        mapView = (MapView) findViewById(R.id.map);
         // 设置控件可见
         mapView.setVisibility(View.VISIBLE);
         // 创建地图（在 activity 执行 onCreate 时执行 mMapView.onCreate(savedInstanceState)
         mapView.onCreate(savedInstanceState);
         // 初始化地图控制器对象
-        if(aMap==null){
-            aMap=mapView.getMap();
+        if (aMap == null) {
+            aMap = mapView.getMap();
         }
-        // 设置高德经纬度（郑州火车站
-        double gd_lng=113.658109;// 经度
-        double gd_lat=34.745803;// 纬度
-        LatLng gd_latlng=new LatLng(gd_lat,gd_lng);
-        MarkerOptions options=new MarkerOptions();
+        // 高德经纬度（郑州火车站
+        double gd_lng = 113.658109;// 经度
+        double gd_lat = 34.745803;// 纬度
+        // 百度经纬度
+        double bd_lng=113.666136;
+        double bd_lat=34.752394;
+        LatLng gd_latlng = new LatLng(gd_lat, gd_lng);
+        LatLng bd_latlng=new LatLng(bd_lat,bd_lng);
+        CoordinateConverter converter=new CoordinateConverter();
+        // CoordType 待转换坐标类型（GPS/Google/Baidu...
+        converter.from(CoordinateConverter.CoordType.BAIDU);
+        // 待转换坐标点
+        converter.coord(bd_latlng);
+        // 执行转换操作
+        LatLng desLatlng=converter.convert();
+        MarkerOptions options = new MarkerOptions();
         options.position(gd_latlng);
-        aMap.moveCamera(CameraUpdateFactory.changeLatLng(gd_latlng));// 中心点
-        aMap.moveCamera(CameraUpdateFactory.zoomTo(12));// 缩放比例
-        aMap.getUiSettings().setZoomControlsEnabled(false);// 隐藏缩放按钮
+        // 显示转换后坐标
+        //options.position(desLatlng);
+        marker=aMap.addMarker(options);
+        marker.showInfoWindow();
+        // 中心点
+        aMap.moveCamera(CameraUpdateFactory.changeLatLng(gd_latlng));
+        // 缩放比例
+        aMap.moveCamera(CameraUpdateFactory.zoomTo(12));
+        // 隐藏缩放按钮
+        aMap.getUiSettings().setZoomControlsEnabled(false);
     }
 
     /**
@@ -57,6 +76,7 @@ public class PointActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        // 重新绘制加载地图（在 activity 执行 onResume 时执行 mapView.onResume()
         mapView.onResume();
     }
 
@@ -66,6 +86,7 @@ public class PointActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+        // 暂停地图的绘制（在 activity 执行 onPause 时执行 mapView.onPause()
         mapView.onPause();
     }
 
@@ -75,6 +96,7 @@ public class PointActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        // 销毁地图（在 activity 执行 onDestroy 时执行 mapView.onDestroy()
         mapView.onDestroy();
     }
 
