850286e0317d9296fdd75bc74b990ee3fd5aa347
cui <cui7dr@gmail.com>
2019-07-17 22:07:23 +0800

    主界面

--- app/src/main/java/com/cui/gaodemapdemo/activity/MainActivity.java
+++ app/src/main/java/com/cui/gaodemapdemo/activity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.cui.gaodemapdemo.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.cui.gaodemapdemo.R;
@@ -11,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, PointActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
