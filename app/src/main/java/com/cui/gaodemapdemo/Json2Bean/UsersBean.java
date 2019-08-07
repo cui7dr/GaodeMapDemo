2b736ebe8b96fe3aa3a006ca3f26204553f2bad3
cui <cui7dr@gmail.com>
2019-08-07 22:04:59 +0800

    用户实体类

---
+++ app/src/main/java/com/cui/gaodemapdemo/Json2Bean/UsersBean.java
@@ -0,0 +1,29 @@
+package com.cui.gaodemapdemo.Json2Bean;
+
+/**
+ * 用户实体类
+ *
+ * @author cui7dr by 2019/08/04
+ */
+
+public class UsersBean {
+    private String code; //返回码
+    private String info; //返回信息
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+}
