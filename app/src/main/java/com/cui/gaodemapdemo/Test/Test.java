c230900ba3ebbfeb21052be052b17dc51d3b79f4
cui <cui7dr@gmail.com>
2019-08-13 22:19:38 +0800

    测试类

--- app/src/main/java/com/cui/gaodemapdemo/Test/Test.java
+++ app/src/main/java/com/cui/gaodemapdemo/Test/Test.java
@@ -48,13 +48,50 @@ public class Test {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        String reuslt = hu.methodPost(headerMap, paramsMap);
+        String result = hu.methodPost(headerMap, paramsMap);
         String login_url = paramsMap.toString().replaceAll(", ", "");
-        System.out.println("HttpUtil 请求返回 json 信息 === " + reuslt);
+        System.out.println("HttpUtil 请求返回 json 信息 === " + result);
         System.out.println("HttpUtil 请求链接 === " + login_url);
     }
 
-    //  2.---- 通过 OkHttp 请求
+    //  2.---- 通过 HttpUtil + Gson 获取返回信息
+    private static void getLogin2ByHttpUtilAddGson() {
+        JSONObject usersJson = new JSONObject();
+        JSONObject urlJson = new JSONObject();
+        usersJson.put("loginname", "yunwei");
+        usersJson.put("password", "xinghe123");
+        urlJson.put("version", Const.version);
+        urlJson.put("method", Const.method_login_xc);
+        urlJson.put("pubKey", "");
+        urlJson.put("sign", "");
+        urlJson.put("data", usersJson);
+        Map<String, String> headerMap = new HashMap<String, String>();
+        Map<String, String> paramsMap = new HashMap<String, String>();
+        paramsMap.put("requestURL", Const.url_xc);
+        try {
+            paramsMap.put("json", URLEncoder.encode(urlJson.toJSONString(), "UTF-8"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String result = hu.methodPost(headerMap, paramsMap);
+        String login_url = paramsMap.toString().replaceAll(", ", "");
+        System.out.println("HttpUtil 请求返回 json 信息 === " + result);
+        System.out.println("HttpUtil 请求链接 === " + login_url);
+        Gson gson = new Gson();
+        UsersBean usersBean = gson.fromJson(result, UsersBean.class);
+        if (usersBean != null) {
+            String code = usersBean.getCode();
+            if ("0".equals(code)) {
+                System.out.println("HttpUtil + Gson 成功时返回 info 信息 === " + usersBean.getInfo());
+            } else {
+                System.out.println("HttpUtil + Gson 失败时返回 info 信息 ===" + usersBean.getInfo());
+            }
+        } else {
+            System.out.println("HttpUtil + Gson 连接超时！");
+        }
+    }
+
+    //  3.---- 通过 OkHttp 请求
     private static void getLogin2ByOkHttp() {
         JSONObject usersJson = new JSONObject();
         JSONObject urlJson = new JSONObject();
@@ -107,15 +144,44 @@ public class Test {
     }
 
     /**
+     * 测试获取点位经纬度方法
+     */
+    //  1.---- 通过 HttpUtil 获取
+    private static void getLatLng1HttpUtil(){
+        JSONObject PointLatLngJson = new JSONObject();
+        JSONObject urlJson = new JSONObject();
+        urlJson.put("version", Const.version);
+        urlJson.put("method", Const.method_pointLatLng_xc);
+        urlJson.put("pubKey", "");
+        urlJson.put("sign", "");
+        urlJson.put("data", PointLatLngJson);
+        Map<String, String> headerMap = new HashMap<String, String>();
+        Map<String, String> paramsMap = new HashMap<String, String>();
+        paramsMap.put("requestURL", Const.url_xc);
+        try {
+            paramsMap.put("json", URLEncoder.encode(urlJson.toJSONString(), "UTF-8"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String result=hu.methodPost(headerMap, paramsMap);
+        String login_url = paramsMap.toString().replaceAll(", ", "");
+        System.out.println("HttpUtil 请求返回 json 信息 === " + result);
+        System.out.println("HttpUtil 请求链接 === " + login_url);
+    }
+
+    /**
      * main 方法
      *
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("1.-------通过 HttpUtil 请求-------");
+        System.out.println("1.-------通过 HttpUtil 请求操作 ++ 登录-------");
         getLogin1ByHttputil();
         System.out.println();
-        System.out.println("2.-------通过 OkHttp 请求-------");
+        System.out.println("2.-------通过 OkHttp 请求操作 ++ 登录-------");
         getLogin2ByOkHttp();
+        System.out.println();
+        System.out.println("1.-------通过 HttpUtil 获取 ++ 点位经纬度-------");
+        getLatLng1HttpUtil();
     }
 }
