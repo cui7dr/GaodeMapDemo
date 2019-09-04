b7ce02973668991180ba69e8077a4148da87f64e
cui <cui7dr@gmail.com>
2019-09-04 21:34:31 +0800

    站点实时数据方法

---
+++ app/src/main/java/com/cui/gaodemapdemo/activity/RealtimeActivity.java
@@ -0,0 +1,22 @@
+package com.cui.gaodemapdemo.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cui.gaodemapdemo.R;
+
+/**
+ * 点位实时数据查看方法
+ *
+ * @author cui7dr by 2019/08/27
+ */
+
+public class RealtimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_realtime);
+    }
+}
