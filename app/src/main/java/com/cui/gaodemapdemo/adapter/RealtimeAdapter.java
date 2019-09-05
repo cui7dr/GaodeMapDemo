d1fd3fe19d023c978c87313345e584b86c98469b
cui <cui7dr@gmail.com>
2019-09-05 20:57:23 +0800

    点位实时数据适配器

---
+++ app/src/main/java/com/cui/gaodemapdemo/adapter/RealtimeAdapter.java
@@ -0,0 +1,40 @@
+package com.cui.gaodemapdemo.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * 点位实时数据适配器
+ *
+ * @author cui7dr by 2019/08/28
+ */
+
+public class RealtimeAdapter extends BaseAdapter {
+
+    private LayoutInflater inflater;
+    private Context context;
+    private String contains;
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
